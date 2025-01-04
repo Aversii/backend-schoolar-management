82b7b1ee7c6d31a34e48f8d590a9a521064668fa
aversii <lucasaversi@gmail.com>
2025-01-04 03:42:56 -0300

    refactored address tests e value object aligning  to value object and clean arch, removing id

--- src/__TESTES__/domain/valueObject/address.test.ts
+++ src/__TESTES__/domain/valueObject/address.test.ts
@@ -19,14 +19,12 @@ describe("Tests for Address Value Object", () => {
     );
 
     expect(address).toBeInstanceOf(Address);
-    expect(address.id).toHaveLength(36);
     expect(address.number).toBeGreaterThan(0);
     expect(address.state).toHaveLength(2);
   });
 
   it("should create a new valid instance of address using with method", () => {
     const address = {
-      id: "f3d2393e-d082-4f3a-b2ba-f2bbd325974f",
       addressLine: "Rua A",
       number: 123,
       city: "Cidade",
@@ -37,13 +35,11 @@ describe("Tests for Address Value Object", () => {
     const addressWith = Address.with(address);
 
     expect(addressWith).toBeInstanceOf(Address);
-    expect(addressWith.id).toHaveLength(36);
     expect(addressWith.state).toHaveLength(2);
   });
 
   it("should validate and compare building methods with missing params for 'number' and 'zipcode'", () => {
     const mockData = {
-      id: "f3d2393e-d082-4f3a-b2ba-f2bbd325974f",
       addressLine: "Rua A",
       number: 17,
       city: "Itapecerica",
@@ -52,10 +48,10 @@ describe("Tests for Address Value Object", () => {
     };
 
     const address = Address.with(mockData);
-    const address2 = Address.create("Rua A", null, "Itapecerica", "SP", null);
+    const address2 = Address.create("Rua B", null, "Itapecerica", "SP", null);
 
     expect(address.number).toBeGreaterThan(0);
-    expect(address.id).not.toBe(address2.id);
+    expect(address.addressLine).not.toEqual(address2.addressLine);
     expect(address2.number).not.toBe(null);
     expect(address2.zipcode).not.toBe(null);
   });
